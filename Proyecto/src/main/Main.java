8538db98f91fdfdd372bad4c4f5c6dfc22869f52
Unknown <unknown@example.com>
2020-04-09 21:43:41 -0500

    Espacios al cargar el proyecto

--- Proyecto/src/main/Main.java
+++ Proyecto/src/main/Main.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import components.dialog.notification.types._NotificationDialogActionFAIL;
 import components.dialog.notification.types._NotificationDialogActionOK;
 import file.FILE;
